df0d7c54f8391b7c7c1557a95f2ac235d1a6649f
Sanket <sanketudaypatil@gmail.com>
2023-05-04 17:10:04 +0530

    first commit

---
+++ src/Shared/loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const loader = () => {
+  return (
+    <div className='load-bar'>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      
+    </div>
+  )
+}
+
+export default loader
